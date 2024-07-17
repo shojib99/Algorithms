e81c1c832d7c1b2faccc3ec95140fcc426861536
Md. Shojib sheikh <128397983+shojib99@users.noreply.github.com>
2024-07-17 15:37:06 +0600

    Depth and Height of a Tree using DFS.cpp

---
+++ Depth and Height of a Tree using DFS.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5 + 5;
+vector<int> adj[N];
+bool visited[N];
+int depth[N];
+int height[N];
+
+void dfs(int u)
+ {
+    visited[u] = true;
+    cout << "Visiting node: " << u << endl;
+
+    for (int v : adj[u])
+        {
+        if (visited[v]) continue;
+        // Depth calculation
+        depth[v] = depth[u] + 1;
+        dfs(v);
+        // Height calculation
+        height[u] = max(height[u], height[v]+1);
+    }
+}
+
+int main()
+ {
+    int n, m;
+    cin >> n >> m;
+
+    for (int i = 0; i < m; i++)
+        {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    dfs(1);
+
+    for (int i = 1; i <= n; i++)
+        {
+        cout << "Depth of Node " << i << ": " << depth[i] << endl;
+    }
+
+    cout << "******************\n";
+
+    for (int i = 1; i <= n; i++)
+        {
+        cout << "Height of Node " << i << ": " << height[i] << endl;
+    }
+
+    return 0;
+}
